675681269f7373ddb87c378fe9078cb6b88c7c39
Ismael <lomik33@hotmail.com>
2017-06-26 21:35:59 -0500

    se agregaron interfaces repository

---
+++ EfRepository/Ef/IRepositoryEf.cs
@@ -0,0 +1,29 @@
+﻿using EfRepository.Ef;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+/// <summary>
+/// Interfaz que agrega definición para especializado para la plataforma .net
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
+public interface IRepositoryEf<TEntity> : IRepository<TEntity> where TEntity:class
+{
+    /// <summary>
+    /// Operacion de guardado por lotes.
+    /// </summary>
+    /// <param name="elements">Enumeracion de objetos</param>
+    /// <param name="saveSkip">Numero de elementos que almacenará en una transacción</param>
+    /// <returns>Total instancias almacenadas</returns>
+    int Save(IEnumerable<TEntity> elements, int saveSkip = 200);
+
+    /// <summary>
+    /// Proyección de un conjunto de entidades en base a un predicado
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns>Conjunto de objetos que cumplen con el predicado</returns>
+    IQueryable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate);
+
+}
---
+++ EfRepository/Models/PersonaPoco.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EfRepository.Models
+{
+    /// <summary>
+    /// Clase de dominio que se traduce a tabla por medio del Orm EF.
+    /// </summary>
+   public class PersonaPoco
+    {
+        public Guid Uuid { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Sexo { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
