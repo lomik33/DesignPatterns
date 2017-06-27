4482e4119403a87e129823573115a0c286bf1c3b
Ismael <lomik33@hotmail.com>
2017-06-27 17:34:08 -0500

    Se agrega la implementación de la operacion de guradado "Save" patrón repository

--- EfRepository/Ef/IRepositoryEf.cs
+++ EfRepository/Ef/IRepositoryEf.cs
@@ -6,11 +6,74 @@ using System.Linq;
 using System.Linq.Expressions;
 
 /// <summary>
-/// Interfaz que agrega definición para especializado para la plataforma .net
+/// Interfaz que define el comportamiento del patron Repository para especializado para la plataforma .Net Framework 4.6
 /// </summary>
 /// <typeparam name="TEntity"></typeparam>
-public interface IRepositoryEf<TEntity> : IRepository<TEntity> where TEntity:class
+public interface IRepositoryEf<TEntity>  where TEntity:class
 {
+    /// <summary>
+    /// Operacion de guardado.
+    /// </summary>
+    /// <param name="entity">entidad de negocio</param>
+    /// <returns></returns>
+    bool Save(TEntity entity);
+    /// <summary>
+    /// Operacion de actualizacion
+    /// </summary>
+    /// <param name="entity">entidad de negocio</param>
+    /// <returns></returns>
+    bool Update(TEntity entity);
+
+
+    /// <summary>
+    /// Operacion de eliminacion
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    bool Delete<TKey>(TKey key);
+
+    /// <summary>
+    /// Operacion de seleccion
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    TEntity Select<TKey>(TKey key);
+
+    /// <summary>
+    /// Operación de seleccion en base a un predicado
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    TEntity ToSelect(Expression<Func<TEntity, bool>> predicate);    
+
+    /// <summary>
+    /// Proyección de un conjunto de entidades en base a un predicado
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns>Conjunto de objetos que cumplen con el predicado</returns>
+    IEnumerable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate);
+
+    /// <summary>
+    /// Devuelve el total de coincidencias en base a un predicado
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    int Count(Expression<Func<TEntity, bool>> predicate);   
+
+
+    /// <summary>
+    /// Operacion para conocer si existen elementos en base a un predicado
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    bool Exists(Expression<Func<TEntity, bool>> predicate);
+
+
+    #region BatchTransactions
+    /*Metodo que definen comportamiento por lotes*/
+
     /// <summary>
     /// Operacion de guardado por lotes.
     /// </summary>
@@ -20,34 +83,17 @@ public interface IRepositoryEf<TEntity> : IRepository<TEntity> where TEntity:cla
     int Save(IEnumerable<TEntity> elements, int saveSkip = 200);
 
     /// <summary>
-    /// Proyección de un conjunto de entidades en base a un predicado
+    /// Operacion que obtiene entidades en base a una carga paginada.
     /// </summary>
+    /// <param name="orderByExpression"></param>
     /// <param name="predicate"></param>
-    /// <returns>Conjunto de objetos que cumplen con el predicado</returns>
-    IEnumerable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate);
+    /// <param name="isOrderByDesc"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="includeExpressions"></param>
+    void ToSelectAll(Expression<Func<TEntity, Int64>> orderByExpression, Expression<Func<TEntity, bool>> predicate, bool isOrderByDesc = false, int pageSize = 200, params Expression<Func<TEntity, object>>[] includeExpressions);
 
+    #endregion
 
-    /// <summary>
-    /// Devuelve el total de coincidencias en base a un predicado
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    int Count(Expression<Func<TEntity, bool>> predicate);
-
-    /// <summary>
-    /// Operación de seleccion en base a un predicado
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    TEntity ToSelect(Expression<Func<TEntity, bool>> predicate);
-
-
-    /// <summary>
-    /// Operacion para conocer si existen elementos en base a un predicado
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    bool Exists(Expression<Func<TEntity, bool>> predicate);
 
 
 }
